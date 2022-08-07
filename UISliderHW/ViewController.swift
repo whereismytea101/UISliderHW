ea0bf98c821f051a0524e2e9a9d83c9d4c13ec24
Samir <imnotajedi@icloud.com>
2022-08-07 21:10:02 +0300

    Initial Commit

---
+++ UISliderHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISliderHW
+//
+//  Created by Самир Кафаров on 07.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
